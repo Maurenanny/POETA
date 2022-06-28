11e73be009b694304487276c81e77c87bd80895d
UlisesIslas <20193tn013@utez.edu.mx>
2022-06-28 18:34:39 -0500

    Angular index & landingpage

--- src/main/java/mx/edu/utez/poeta/ServletInitializer.java
+++ src/main/java/mx/edu/utez/poeta/ServletInitializer.java
@@ -3,7 +3,7 @@ package mx.edu.utez.poeta;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializaer extends SpringBootServletInitializer {
+public class ServletInitializer extends SpringBootServletInitializer {
     
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
